c6036243e72fb0f504e7488c39bb55b5c8038356
Bryce Schmidtchen <BryceSchmidtchen@gmail.com>
2017-02-19 00:08:05 -0800

    Facebook's Version

--- src/config.js
+++
@@ -1,6 +0,0 @@
-var messengerPageToken = process.env.MESSENGER_TOKEN || 'EAAaaynZCAqP0BANTwHZBxzNHco1mEwvn9mlymCwNheCEqmy2clDCUs8Y9oLae6EChj0lTSmuU0wt6VFDUgII1SpFP8p64ZC3usC58F7ZA7c8pYZCdhoVTbM5NVousoK9tZBnwwDQKtkgcBi6lpBSPlSFjEZCfBV7SZAvH2qn41kQ0wZDZD'; 
-
-module.exports = {
-  messengerPageToken: messengerPageToken,
-  messengerVerifyToken: 'TOKEN'
-};
