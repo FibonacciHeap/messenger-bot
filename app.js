0b7ed00942120896bbbb0259edae1895ec37a383
Bryce Schmidtchen <BryceSchmidtchen@gmail.com>
2017-02-19 06:44:51 -0800

    testing conversations

--- app.js
+++ app.js
@@ -290,7 +290,7 @@ function receivedMessage(event) {
 
 
 
-  if (messageText) {
+  //if (messageText) {
 
     // If we receive a text message, check to see if it matches any special
     // keywords and send back the corresponding example. Otherwise, just echo
