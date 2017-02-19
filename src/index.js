afb027f95cae606344d721ad5556e5884eaa61de
Bryce Schmidtchen <BryceSchmidtchen@gmail.com>
2017-02-18 21:15:09 -0800

    reverted to without app.listen mod

--- src/index.js
+++ src/index.js
@@ -13,10 +13,4 @@ app.post('/', messenger.handleMessages);
 
 app.listen(app.get('port'), () => {
   console.log('Server listening at port', app.get('port'));
-  curl -X POST -H "Content-Type: application/json" -d '{
-  "setting_type":"greeting",
-  "greeting":{
-    "text":"Hi {{user_first_name}}! Welcome to My Company!"
-  }
-}' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=EAAaaynZCAqP0BANTwHZBxzNHco1mEwvn9mlymCwNheCEqmy2clDCUs8Y9oLae6EChj0lTSmuU0wt6VFDUgII1SpFP8p64ZC3usC58F7ZA7c8pYZCdhoVTbM5NVousoK9tZBnwwDQKtkgcBi6lpBSPlSFjEZCfBV7SZAvH2qn41kQ0wZDZD"
 });
